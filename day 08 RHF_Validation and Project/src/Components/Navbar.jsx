18e7a032e7b0fd9a37ef4631258199d7534ee429
manishmandal0611 <manishmandal0611@gmail.com>
2026-09-05 22:51:04 +0530

    update all the folder and files name

--- day 08 RHF_Validation and Project/src/Components/Navbar.jsx
+++ day 08 RHF_Validation and Project/src/Components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Navbar = () => {
+const Navbar = ({setToggle}) => {
   return (
     <div className='p-4 flex rounded items-center justify-between bg-black text-white'>
       <div>
@@ -8,11 +8,16 @@ const Navbar = () => {
         src="https://static.vecteezy.com/system/resources/previews/000/439/863/non_2x/vector-users-icon.jpg" alt="" />
       </div>
       <div className='flex gap-6 font-semibold text-lg'>
-        <p>User</p>
+        <p>Home</p>
         <p>About</p>
         <p>Contacts</p>
       </div>
-      <button className='bg-white rounded cursor-pointer text-black px-4 py-2 font-semibold hover:bg-gray-200'>Create user</button>
+        <button
+        onClick={() => setToggle((prev) => !prev)}
+        className="p-2 bg-blue-700 text-white cursor-pointer rounded"
+      >
+        Create user
+      </button>
     </div>
   )
 }
--- day 08 RHF_Validation and Project/src/Components/Usercard.jsx
+++ day 08 RHF_Validation and Project/src/Components/Usercard.jsx
@@ -1,23 +1,33 @@
 import React from 'react'
 
-const Usercard = () => {
+const Usercard = ({ user, setToggle }) => {
   return (
-    <div className="h-30 p-4 border-white rounded">
-      <div className="h-60">
-        <img className='object-fit h-full'
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTv1pkbKtt3GVsy318SGRqPJ1vphR-w1TeIGcrxN-sb-vnD9PCwTmPVSRE&s=10" alt="" />
+    <div className="p-4 border border-white bg-black rounded flex flex-col gap-2">
+      <div className="h-60 w-40">
+        <img 
+        className='object-cover h-full w-full rounded-xl'
+        src={user.image} 
+        alt="" 
+        />
       </div>
-      <div>
-        <h1>Name</h1>
-        <p>Email</p>
-        <p>Contact</p>
+      <div className='flex flex-col gap-1'>
+        <h1>{user.name}</h1>
+        <p className="text-sm">{user.email}</p>
+        <p className="text-sm">{user.contact}</p>
       </div>
-      <div>
-        <button className='bg-red-500 text-white px-4 py-2 rounded-md'>Delete</button>
-        <button className='bg-blue-500 text-white px-4 py-2 rounded-md'>Update</button>
+       <div className="flex w-full justify-between gap-4">
+        <button
+          onClick={() => setToggle((prev) => !prev)}
+          className="bg-yellow-700 text-white py-2 px-3 rounded"
+        >
+          Update
+        </button>
+        <button className="bg-red-700 text-white py-2 px-3 rounded">
+          Delete
+        </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Usercard
+export default Usercard;
