1b5e391c16da12f62ad925c9ac0788bb98512003
manishmandal0611 <manishmandal0611@gmail.com>
2026-09-07 11:53:38 +0530

    upload all files

--- day 10 ContextApi/src/Components/Navbar.jsx
+++ day 10 ContextApi/src/Components/Navbar.jsx
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { MyShop } from "../context/MyWebsite";
 
 const Navbar = () => {
-  return (
-    <div>
-      <div>
-        <h1>Navbar</h1>
-      </div>
-    </div>
-  )
-}
+  let { setIsCartOpen } = useContext(MyShop);
 
-export default Navbar
+  return (
+    <div className="bg-black rounded p-5 flex items-center justify-between">
+      <div className="text-2xl font-bold text-white">logo</div>
+      <div className="flex gap-10 text-xl">
+        <p onClick={() => setIsCartOpen(false)} className="text-white cursor-pointer">
+          Home
+        </p>
+        <p onClick={() => setIsCartOpen(true)} className="text-white cursor-pointer">
+          Cart
+        </p>
+      </div>
+      <button className="bg-white text-black px-4 py-2 rounded">Login</button>
+    </div>
+  );
+};
+
+export default Navbar;
